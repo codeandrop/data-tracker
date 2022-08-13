ecf4e6e0571d438b3bb9e2145a7dd8c9068f2231
Andres Osorio <and285@gmail.com>
2022-08-13 14:28:15 -0400

    handle error in insert operations

--- src/models/metrics_prices.py
+++ src/models/metrics_prices.py
@@ -1,3 +1,6 @@
+import sqlite3
+
+
 class MetricsPricesModel():
     def __init__(self, conn):
         self.conn = conn
@@ -24,14 +27,24 @@ class MetricsPricesModel():
 
     def insert(self, record):
         cur = self.conn.cursor()
-        cur.execute(
-            "INSERT INTO metrics_prices(metric_id,price,created_at,updated_at) VALUES(?,?,?,?);", record)
-        self.conn.commit()
-        return cur.lastrowid
+        try:
+            cur.execute(
+                "INSERT INTO metrics_prices(metric_id,price,created_at,updated_at) VALUES(?,?,?,?);", record)
+            self.conn.commit()
+            return cur.lastrowid
+        except sqlite3.Error as error:
+            print(f"Failed to insert a row in metrics_prices {error}")
+
+        return -1
 
     def insert_many(self, rows):
         cur = self.conn.cursor()
-        cur.executemany(
-            "INSERT INTO metrics_prices(metric_id,price,created_at,updated_at) VALUES(?,?,?,?);", rows)
-        self.conn.commit()
-        return cur.rowcount
+        try:
+            cur.executemany(
+                "INSERT INTO metrics_prices(metric_id,price,created_at,updated_at) VALUES(?,?,?,?);", rows)
+            self.conn.commit()
+            return cur.rowcount
+        except sqlite3.Error as error:
+            print(f"Failed to insert many rows in metrics_prices {error}")
+
+        return -1
