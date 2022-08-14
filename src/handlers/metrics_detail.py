bb88bd88a33fef88399a0d714f45ed4753892094
Andres Osorio <and285@gmail.com>
2022-08-14 14:28:45 -0400

    handle metric detail not found

--- src/handlers/metrics_detail.py
+++ src/handlers/metrics_detail.py
@@ -8,6 +8,11 @@ class MetricsDetail(RequestHandler):
         metrics_view = MetricsView()
         metrics_service = MetricsService(self.application.conn)
         metric = metrics_service.fetch_by_id(metric_id)
+        if metric is None:
+            self.set_status(404)
+            self.finish({"reason": "Resource not found"})
+            return
+
         metric_output = metrics_view.metric_item(metric)
         response = {
             "metric": metric_output
